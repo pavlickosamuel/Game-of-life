d5898ddf2fe4626476023bf3f4007e734b388284
pavlickosamuel <pavlickosamuel@gmail.com>
2026-05-13 09:22:30 +0200

    treti

--- gameoflife.py
+++ gameoflife.py
@@ -10,8 +10,10 @@ def create_dishes(height, width):
     for i in range (height):
         dish2.append([0] * width)
         dish1.append([0] * width)
+
     y = 0
     x = 0
+    
     for line in fr:
         x = 0
         for char in line.strip():
@@ -20,5 +22,26 @@ def create_dishes(height, width):
             x += 1
         y += 1
 
+def get_neighbors(dish, x, y):
+    neighbors = 0
+    if x > 0 and y > 0 and dish[y-1][x-1] == 1:
+            neighbors += 1
+    if y > 0 and dish[y-1][x+1] == 1:
+            neighbors += 1
+    if x > 0 and dish[y][x-1] == 1:
+            neighbors += 1
+    if dish[y][x+1] == 1:
+            neighbors += 1
+    if x > 0 and dish[y+1][x-1] == 1:
+            neighbors += 1
+    if dish[y+1][x] == 1:
+            neighbors += 1
+    if dish[y+1][x+1] == 1:
+            neighbors += 1
+    if x > 0 and dish[y+1][x-1] == 1:
+            neighbors += 1
+    print(neighbors)
+
+
 create_dishes(height, width)
-print(dish2)
+get_neighbors(dish1, 21, 2)
