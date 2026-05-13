5e4fee697eaf0b5229a72896ab81c3b17428b355
pavlickosamuel <pavlickosamuel@gmail.com>
2026-05-13 08:54:37 +0200

    druhy

--- gameoflife.py
+++ gameoflife.py
@@ -7,9 +7,18 @@ dish2 = []
 
 def create_dishes(height, width):
     global dish1, dish2
-    #idem spraviť ten prázdny
     for i in range (height):
         dish2.append([0] * width)
+        dish1.append([0] * width)
+    y = 0
+    x = 0
+    for line in fr:
+        x = 0
+        for char in line.strip():
+            if char != "-":
+                dish1[y][x] = 1
+            x += 1
+        y += 1
 
 create_dishes(height, width)
 print(dish2)
