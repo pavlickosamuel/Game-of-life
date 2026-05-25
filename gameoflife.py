c8e7f0a699b0a9e95fb1761d50f162ebedf807aa
pavlickosamuel <pavlickosamuel@gmail.com>
2026-05-25 09:17:57 +0200

    osmy

--- gameoflife.py
+++ gameoflife.py
@@ -24,26 +24,34 @@ def create_dishes(height, width):
 
 def get_neighbors(dish, x, y):
     neighbors = 0
-    if x > 0 and y > 0 and dish[y-1][x-1] == 1:
-            neighbors += 1
-    if y > 0 and x < width - 1 and dish[y-1][x+1] == 1:
-            neighbors += 1
-    if x > 0 and dish[y][x-1] == 1:
-            neighbors += 1
-    if x < width - 1 and dish[y][x+1] == 1:
-            neighbors += 1
-    if x > 0 and y < height - 1 and dish[y+1][x-1] == 1:
-            neighbors += 1
-    if y < height - 1 and dish[y+1][x] == 1:
-            neighbors += 1
-    if y < height - 1 and x < width - 1 and dish[y+1][x+1] == 1:
-            neighbors += 1
-    if x > 0 and y < height - 1 and dish[y+1][x-1] == 1:
-            neighbors += 1
+    if y > 0:
+        if x > 0 and dish[y-1][x-1] == 1: neighbors += 1
+        if dish[y-1][x] == 1: neighbors += 1
+        if x < width - 1 and dish[y-1][x+1] == 1: neighbors += 1
+    
+    if x > 0 and dish[y][x-1] == 1: neighbors += 1
+    if x < width - 1 and dish[y][x+1] == 1: neighbors += 1
+    
+    if y < height - 1:
+        if x > 0 and dish[y+1][x-1] == 1: neighbors += 1
+        if dish[y+1][x] == 1: neighbors += 1
+        if x < width - 1 and dish[y+1][x+1] == 1: neighbors += 1
+    
     print(neighbors)
 
-#def copy_dishes(source, destination): kopirovat miskz medyi sebou kopirovanie sa musi striedat
+state = True
+def copy_dishes(source, destination): #def copy_dishes(source, destination): kopirovat misky medzi sebou kopirovanie sa musi striedat
+        global state
+        if state == True:
+                for y in range(height):
+                        for x in range(width):
+                                destination[y][x] = source[y][x]
+                        state = False
+        else:
+                for y in range(height):
+                        for x in range(width):
+                                source[y][x] = destination[y][x]
+                        state = True
         
-
 create_dishes(height, width)
-get_neighbors(dish1, 0, 0)
+get_neighbors(dish1, 21, 1)
